6ae12be70f29ce3b2dbbc1a63b84da4c1a78e4dc
26messias26 <messias26severino@gmail.com>
2023-03-31 10:19:27 -0300

    Correção da classificação online e a remoção do arquivo locadora

--- src/classificacoes/ClassificacaoOnline.java
+++ src/classificacoes/ClassificacaoOnline.java
@@ -1,24 +1,23 @@
 package classificacoes;
 
 public class ClassificacaoOnline extends Classificacao {
-    private static final int CÓDIGO_DE_PREÇO_ONLINE = 4;
-    private static final double PREÇO_DIÁRIO = 4.0;
-    private static final double PREÇO_DIÁRIO_PROMOCIONAL = 3.0;
+    private static final int CODIGO_DE_PRECO_ONLINE = 4;
+    private static final double PRECO_DIARIO = 4.0;
+    private static final double PRECO_DIARIO_PROMOCIONAL = 3.0;
     private static final int DIAS_PROMOCIONAIS = 3;
 
     @Override
     public int getCódigoDePreço() {
-        return CÓDIGO_DE_PREÇO_ONLINE;
+        return CODIGO_DE_PRECO_ONLINE;
     }
 
     @Override
     public double getValorDoAluguel(int diasAlugados) {
         double valorDoAluguel;
         if (diasAlugados > DIAS_PROMOCIONAIS) {
-            valorDoAluguel = DIAS_PROMOCIONAIS * PREÇO_DIÁRIO_PROMOCIONAL
-                    + (diasAlugados - DIAS_PROMOCIONAIS) * PREÇO_DIÁRIO;
+            valorDoAluguel = diasAlugados * PRECO_DIARIO_PROMOCIONAL;
         } else {
-            valorDoAluguel = diasAlugados * PREÇO_DIÁRIO_PROMOCIONAL;
+            valorDoAluguel = diasAlugados * PRECO_DIARIO;
         }
         return valorDoAluguel;
     }
